21d01b96714efbb7f4b5cb01a757edb69efef07e
alizaibtech1-bit <alizaibtech1@gmail.com>
2026-06-04 13:09:09 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use('/api/products', require('./routes/products'));
 app.use('/api/orders', require('./routes/orders'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/oauth', require('./routes/oauth'));
+app.use('/api/seed', require('./routes/seed'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
